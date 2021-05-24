795258540c0bc1249c9ea73ab2868e5c463f5bf2
Rosen Spiridonov <rspiridonov3@gmail.com>
2021-05-24 14:44:17 +0300

    Add HttpServer and IHttpServer

--- SUS.HTTP/HttpServer.cs
+++ SUS.HTTP/HttpServer.cs
@@ -10,19 +10,11 @@ namespace SUS.HTTP
 {
     public class HttpServer : IHttpServer
     {
-        IDictionary<string, Func<HttpRequest, HttpResponse>> routeTable =
-            new Dictionary<string, Func<HttpRequest, HttpResponse>>();
+        List<Route> routeTable;
 
-        public void AddRoute(string path, Func<HttpRequest, HttpResponse> action)
+        public HttpServer(List<Route> routeTable)
         {
-            if (routeTable.ContainsKey(path))
-            {
-                routeTable[path] = action;
-            }
-            else
-            {
-                routeTable.Add(path, action);
-            }
+            this.routeTable = routeTable;
         }
 
         public async Task StartAsync(int port)
@@ -74,10 +66,11 @@ namespace SUS.HTTP
 
                     HttpResponse response;
 
-                    if (routeTable.ContainsKey(request.Path))
+                    var route = this.routeTable.FirstOrDefault(x => x.Path == request.Path);
+
+                    if (route != null)
                     {
-                        var action = this.routeTable[request.Path];
-                        response = action(request);
+                        response = route.Action(request);
                     }
                     else
                     {
